9635460ee473d863ac4bf66154ed23eb9f48fbbe
barabarinov <barabarinov@gmail.com>
2024-07-24 18:55:00 +0300

    Implemented generate_audio function

---
+++ app/audio.py
@@ -0,0 +1,49 @@
+import torch
+
+import torchaudio
+from riffusion.spectrogram_params import SpectrogramParams
+from riffusion.streamlit import util as streamlit_util
+
+
+def generate_audio(
+    prompt: str,
+    num_inference_steps: int,
+    guidance: float,
+    negative_prompt: str,
+    seed: int,
+    width: int,
+    output_path: str,
+    checkpoint: str,
+    device: str,
+    scheduler: str,
+    extension: str,
+    params: SpectrogramParams,
+) -> None:
+    with torch.no_grad():
+        image = streamlit_util.run_txt2img(
+            prompt=prompt,
+            num_inference_steps=num_inference_steps,
+            guidance=guidance,
+            negative_prompt=negative_prompt,
+            seed=seed,
+            width=width,
+            height=512,
+            checkpoint=checkpoint,
+            device=device,
+            scheduler=scheduler,
+        )
+    print(f"✅ Finished running run_txt2img")
+    segment = streamlit_util.audio_segment_from_spectrogram_image(
+        image=image,
+        params=params,
+        device=device,
+    )
+    print(f"✅ Finished running audio_segment_from_spectrogram_image")
+
+    torchaudio.save(output_path, segment, 44100)
+
+    streamlit_util.display_and_download_audio(
+        segment, name=f"{prompt.replace(' ', '_')}_{seed}", extension=extension
+    )
+
+    print(f"✅ Audio generated and saved to {output_path}")
